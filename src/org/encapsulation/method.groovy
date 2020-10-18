9f4685ce0fe38f1c406a0f359b890735e5a494e7
yehe <yinhengyue@qq.com>
2020-10-18 22:40:31 +0800

    no message

--- src/org/encapsulation/method.groovy
+++ src/org/encapsulation/method.groovy
@@ -9,44 +9,6 @@ def gitscm(branchName,url){
     userRemoteConfigs: [[credentialsId: CRED_ID, url: url]]])
 }
 
-def notice(state,ROBOT_ID,JOB_NAME,JOB_URL,BUILD_USER,MESSAGE_INFO){
-    // 判断状态
-    if (state == "success"){
-        res = "- 状态：<font color=#52C41A>${currentBuild.currentResult}</font>"
-    } else if (state == "error"){
-        res = "- 状态：<font color=#F5222D>${currentBuild.currentResult}</font>"
-    }
-    // 日志路径替换
-    JOB_URL = JOB_URL.replaceAll("http://jenkins:8080","https://jenkins.inf.funplus.social")
-    BUILD_URL = BUILD_URL.replaceAll("http://jenkins:8080","https://jenkins.inf.funplus.social")
-
-    // 钉钉插件
-    JOB_URL = 
-    dingTalk (
-        robot: ROBOT_ID,
-        type: 'ACTION_CARD',
-        title: 'Deploy',
-        text: [
-            "[${JOB_NAME}](${JOB_URL})\n",
-            '---',
-            "- 任务：[#${BUILD_ID}](${BUILD_URL}/console)",
-            "${res}",
-            "- 执行人：${BUILD_USER}",
-            "- 持续时间：${currentBuild.durationString}",
-            "- 消息：${MESSAGE_INFO}",
-            "- 私有仓库地址： [点我](http://nexus.inf.funplus.social/#browse/browse:android-social-platform-snapshot)\n"
-        ],
-        btns: [
-        [
-            title: 'Sonar',
-            actionUrl: "https://sonarqube.ftsview.com/"
-        ],
-        [
-            title: '私有仓库',
-            actionUrl: "http://nexus.inf.funplus.social/#browse/browse:android-social-platform-snapshot"
-        ]
-    ])
-}
 
 def defaultConf(){
     // 定义配置文件
