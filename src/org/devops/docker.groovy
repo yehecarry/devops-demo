db8a08f725d59dcfd3da43a359a662231fb730e3
yehe <yinhengyue@qq.com>
2020-10-18 22:55:28 +0800

    no message

--- src/org/devops/docker.groovy
+++ src/org/devops/docker.groovy
@@ -5,7 +5,7 @@ def PushHarborDocker(docker_image_name,docker_image_tag,harbor_url,harbor_group)
         
         docker_image = "${docker_image_name}:${docker_image_tag}"
         sh """
-        docker login -u ${username} -p ${password} https://${harbor_url}
+        docker login -u ${username} -p ${password} https://${harbor_url}/v1
         docker build -t ${docker_image} .
         docker tag ${docker_image} ${harbor_url}/${harbor_group}/${docker_image}
         docker push ${harbor_url}/${harbor_group}/${docker_image}
